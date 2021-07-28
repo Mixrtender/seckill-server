b26ffebb57702300403e1478af77055d4cc46b52
lanxw <lanxiongwei@wolfcode.cn>
2021-07-28 17:30:04 +0800

    更新

--- shop-parent/shop-provider-api/seckill-api/src/main/java/cn/wolfcode/domain/PayLog.java
+++ shop-parent/shop-provider-api/seckill-api/src/main/java/cn/wolfcode/domain/PayLog.java
@@ -12,9 +12,8 @@ import lombok.Setter;
 public class PayLog {
     public static final int PAY_TYPE_ONLINE = 0;//在线支付
     public static final int PAY_TYPE_INTERGRAL = 1;//积分支付
-    private String tradeNo;//订单编号
-    private String outTradeNo;//支付流水号
-    private String notifyTime;//订单修改时间
+    private String orderNo;//订单编号
+    private String payTime;//支付时间
     private String totalAmount;//交易数值
     private int payType;//支付类型
 }
