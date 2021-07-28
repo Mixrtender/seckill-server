dd4dc4ee4097f979da716d65d124a3eec7a2a000
lanxw <lanxiongwei@wolfcode.cn>
2021-07-28 17:59:19 +0800

    更新

--- shop-parent/shop-provider/intergral-server/src/main/java/cn/wolfcode/service/impl/UsableIntegralServiceImpl.java
+++ shop-parent/shop-provider/intergral-server/src/main/java/cn/wolfcode/service/impl/UsableIntegralServiceImpl.java
@@ -1,20 +1,10 @@
 package cn.wolfcode.service.impl;
 
-import cn.wolfcode.common.exception.BusinessException;
-import cn.wolfcode.domain.AccountLog;
-import cn.wolfcode.domain.AccountTransaction;
-import cn.wolfcode.domain.OperateIntergralVo;
-import cn.wolfcode.mapper.AccountLogMapper;
 import cn.wolfcode.mapper.AccountTransactionMapper;
 import cn.wolfcode.mapper.UsableIntegralMapper;
 import cn.wolfcode.service.IUsableIntegralService;
-import cn.wolfcode.web.msg.IntergralCodeMsg;
-import com.alibaba.fastjson.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Date;
 
 /**
  * Created by lanxw
@@ -25,7 +15,4 @@ public class UsableIntegralServiceImpl implements IUsableIntegralService {
     private UsableIntegralMapper usableIntegralMapper;
     @Autowired
     private AccountTransactionMapper accountTransactionMapper;
-    @Autowired
-    private AccountLogMapper accountLogMapper;
-
 }
