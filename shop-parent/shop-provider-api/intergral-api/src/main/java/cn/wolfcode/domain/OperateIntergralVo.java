3fe470dec33451fc565dfcaa08db3992110fdc1f
lanxw <lanxiongwei@wolfcode.cn>
2021-07-28 17:48:08 +0800

    更新

--- shop-parent/shop-provider-api/intergral-api/src/main/java/cn/wolfcode/domain/OperateIntergralVo.java
+++ shop-parent/shop-provider-api/intergral-api/src/main/java/cn/wolfcode/domain/OperateIntergralVo.java
@@ -11,8 +11,6 @@ import java.io.Serializable;
  */
 @Setter@Getter
 public class OperateIntergralVo implements Serializable {
-    private String pk;//业务主键
     private Long value;//此次积分变动数值
-    private String info;//备注
     private Long userId;//用户ID
 }
